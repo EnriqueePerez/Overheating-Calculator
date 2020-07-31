cf15d7741de4f5f2a6f661210539ff0a4ec87816
Enrique Perez <enrike_084@hotmail.com>
2020-07-31 00:43:13 -0500

    Added functionality to Main component
    Now, we can calculate with R404 refrigerant
    We avoid entering empty values
    The data is gathered in a form ready to send

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -3,41 +3,88 @@ import '../assets/styles/components/Main.scss';
 import {
   validateStopPressureR404,
   validateCutPressureR404,
+  validateOverheatingTemperature,
+  calculation,
 } from '../utils/validation';
 
 const Main = (props) => {
   const [approved, setApproved] = useState(false);
+  const [tubeTemperature, setTubeTemperature] = useState(0);
+  const [saturationTemperature, setSaturationTemperature] = useState(0);
+  const [overheatingTemperature, setOverheatingTemperature] = useState(0);
+  const [form, setValues] = useState({});
+  const handleInput = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
   const validationCutPressure = (e) => {
     validateCutPressureR404(e);
     if (validateCutPressureR404(e)) {
       setApproved(true);
+      handleInput(e);
     } else {
       setApproved(false);
     }
   };
+
   const validationStopPressure = (e) => {
     validateStopPressureR404(e);
     if (validateStopPressureR404(e)) {
       setApproved(true);
+      handleInput(e);
     } else {
       setApproved(false);
     }
   };
-  const tubeTemperature = 21;
-  const saturationTemperature = 25;
-  const overheatingTemperature = 22;
+
+  const validationOverheating = (e) => {
+    validateOverheatingTemperature(e);
+    handleInput(e);
+  };
+
+  const unit = 'Conservación';
+  const refrigerant = 'R404a';
   // /?unit=conservacion&refrigerant=R404a
+
+  const calculate = () => {
+    if (form.Presion === undefined || form.Resistencia === undefined) {
+      alert('Faltan campos por rellenar');
+    } else if (form.Presion === '' || form.Resistencia === '') {
+      alert('Faltan campos por rellenar');
+    } else {
+      const operation = calculation(
+        refrigerant,
+        form.Presion,
+        form.Resistencia
+      );
+      operation &&
+        (setSaturationTemperature(operation.saturationTemp),
+        setTubeTemperature(operation.tubeTemp),
+        setOverheatingTemperature(operation.overheatTemp),
+        setValues({
+          ...form,
+          'Temperatura de Saturacion': operation.saturationTemp,
+          'Temperatura del Tubo': operation.tubeTemp,
+          'Temperatura de Sobrecalentamiento': operation.overheatTemp,
+        }));
+      console.log(form);
+    }
+  };
+
   return (
     <>
       <header>
         <h2>Calculadora de sobrecalentamiento</h2>
-        <p>Conservacion</p>
-        <p>R404a</p>
+        <p>{unit}</p>
+        <p>{refrigerant}</p>
       </header>
       <form>
         <div className='field-container'>
           <h3>Presión de corte del presostato</h3>
           <input
+            name='Presion de corte del presostato'
             id='cutPressure'
             type='number'
             placeholder='PSI'
@@ -47,6 +94,7 @@ const Main = (props) => {
         <div className='field-container'>
           <h3>Presión de arranque del presostato</h3>
           <input
+            name='Presion de arranque del presostato'
             id='stopPressure'
             type='number'
             placeholder='PSI'
@@ -56,26 +104,42 @@ const Main = (props) => {
         <div className='calculator'>
           <div className='field-container'>
             <h3>Presión</h3>
-            <input type='number' placeholder='PSI' />
+            <input
+              name='Presion'
+              type='number'
+              placeholder='PSI'
+              onChange={handleInput}
+            />
           </div>
           <div className='field-container'>
             <h3>Resistencia</h3>
-            <input type='number' placeholder='Ω' />
+            <input
+              name='Resistencia'
+              type='number'
+              placeholder='Ω'
+              onChange={handleInput}
+            />
           </div>
           <div className='field-container'>
-            <button type='submit'>Calcular</button>
+            <button type='button' onClick={calculate}>
+              Calcular
+            </button>
           </div>
           <div className='temperature-container'>
             <h3>Temperatura del tubo</h3>
-            <p>{`${tubeTemperature}°C`}</p>
+            <p name='Temperatura del tubo' onChange={handleInput}>
+              {`${tubeTemperature}°C`}
+            </p>
           </div>
           <div className='temperature-container'>
             <h3>Temperatura de saturación</h3>
-            <p>{`${saturationTemperature}°C`}</p>
+            <p name='Temperatura de saturación'>
+              {`${saturationTemperature}°C`}
+            </p>
           </div>
           <div className='main-temperature-container'>
             <h2>Temperatura de sobrecalentamiento</h2>
-            <p>{`${overheatingTemperature}°C`}</p>
+            <p id='overheatingTemp'>{`${overheatingTemperature}°C`}</p>
           </div>
         </div>
       </form>
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,21 +1,60 @@
 export const validateCutPressureR404 = (e) => {
-  if (e.target.value >= 54 && e.target.value <= 66) {
-    document.querySelector('#cutPressure').style.backgroundColor = '#88fc88';
-    console.log('APROVADO');
+  if (e.target.value >= 63 && e.target.value <= 77) {
+    document.querySelector('#cutPressure').style.backgroundColor = '#88fc88'; //green
+    // console.log('APROVADO');
     return true;
   }
-  document.querySelector('#cutPressure').style.backgroundColor = '#fa6b6b';
-  console.log('RECHAZADO');
+  document.querySelector('#cutPressure').style.backgroundColor = '#fa6b6b'; //red
+  // console.log('RECHAZADO');
   return false;
 };
 
 export const validateStopPressureR404 = (e) => {
-  if (e.target.value >= 27 && e.target.value <= 33) {
-    document.querySelector('#stopPressure').style.backgroundColor = '#88fc88';
-    console.log('APROVADO');
+  if (e.target.value >= 36 && e.target.value <= 44) {
+    document.querySelector('#stopPressure').style.backgroundColor = '#88fc88'; //green
+    // console.log('APROVADO');
     return true;
   }
-  document.querySelector('#stopPressure').style.backgroundColor = '#fa6b6b';
-  console.log('RECHAZADO');
+  document.querySelector('#stopPressure').style.backgroundColor = '#fa6b6b'; //red
+  // console.log('RECHAZADO');
   return false;
+};
+
+export const validateOverheatingTemperature = (e) => {
+  if (e.target.value >= 12 && e.target.value <= 17) {
+    document.querySelector('#overheatingTemp').style.backgroundColor =
+      '#88fc88'; //green
+    // console.log('APROVADO');
+    return true;
+  }
+};
+
+export const calculation = (refrigerant, pressure, resistance) => {
+  switch (refrigerant) {
+    case 'R404a': {
+      const A = 4.02423;
+      const B = 786.645;
+      const C = -30.093;
+
+      const psi = pressure;
+      const ohms = resistance;
+
+      const P = psi / 14.504 + 1.04; //converting psi to bar
+
+      const T = B / (A - Math.log10(P)) - C; //main operation
+
+      const saturationTemp = (T - 273.15).toFixed(2); //converting kelvin to celsius
+      const tubeTemp = ((ohms - 1000) / 3.9).toFixed(2);
+      const overheatTemp = (
+        parseFloat(tubeTemp) - parseFloat(saturationTemp)
+      ).toFixed(2);
+      const data = { saturationTemp, tubeTemp, overheatTemp };
+      return data;
+    }
+    case 'R22': {
+      const A = 6.7577;
+      const B = 740.39;
+      const C = 231.86;
+    }
+  }
 };
